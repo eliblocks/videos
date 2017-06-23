097517b9fd13246cfcc3312109a5308f45a40456
eli block <eliblock@yahoo.com>
2017-06-23 15:53:42 -0700

    fix braintree initialization

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,9 @@
-Braintree::Configuration.environment = :sandbox
+if Rails.env == 'development' || Rails.env == 'test'
+  Braintree::Configuration.environment = :sandbox
+elsif Rails.env == 'production'
+  Braintree::Configuration.environment = :production
+end
+
 Braintree::Configuration.merchant_id = ENV["BRAINTREE_ID"]
 Braintree::Configuration.public_key = ENV["BRAINTREE_PUBLIC_KEY"]
 Braintree::Configuration.private_key = ENV["BRAINTREE_SECRET"]
