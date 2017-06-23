2c81a4b3af2d6103fb2afb1f2c07671c282b5542
eli block <eliblock@yahoo.com>
2017-06-22 17:48:20 -0700

    edit migration files?

--- db/migrate/20170518190443_create_charges.rb
+++ db/migrate/20170518190443_create_charges.rb
@@ -1,7 +1,7 @@
 class CreateCharges < ActiveRecord::Migration[5.1]
   def change
     create_table :charges do |t|
-      t.string :stripe_charge_id
+      t.string :gateway_charge_id
       t.integer :amount
       t.integer :amount_refunded
       t.string :balance_transaction
@@ -10,7 +10,7 @@ class CreateCharges < ActiveRecord::Migration[5.1]
       t.string :currency
       t.string :description
       t.boolean :success
-      t.references :source, foreign_key: true
+
       t.references :user, foreign_key: true
       t.timestamps
     end
