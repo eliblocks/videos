20e361c3e48d1e145159e8f4d964556a944abc74
eli block <eliblock@yahoo.com>
2017-09-21 15:15:33 -0700

    sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :charges
@@ -19,10 +19,8 @@ class User < ApplicationRecord
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.full_name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    user.image = auth.info.image
+    user.skip_confirmation!
   end
 end
 
